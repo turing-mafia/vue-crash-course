445c184fceed368235adfe509fe32270442acd96
I_Jemin <i_jemin@icloud.com>
2019-10-13 00:54:19 +0900

    Router 적용

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,18 @@
 <template>
   <div id="app">
-    <Header />
-    <AddTodo v-on:add-todo="addTodo"/>
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+    <Header /> 
+    <router-view/>
   </div>
 </template>
 
 <script>
 import Header from './components/layout/Header';
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
-import axios from 'axios';
-
 export default {
-  name: 'app',
+  name:"app",
   components: {
-    Header,
-    Todos,
-    AddTodo
-  },
-  data() {
-    return {
-      todos: []
-    }
-  },
-  methods: {
-    deleteTodo(id) {
-      axios.delete('https://jsonplaceholder.typicode.com/todos/${id}').then(
-        res => this.todos = this.todos.filter(todo => todo.id !== id)
-      ).catch(err => console.log(err));
-    },
-    addTodo(newTodo) {
-      const { title, completed } = newTodo;
-      axios.post('https://jsonplaceholder.typicode.com/todos', {title, completed}).then(
-        res => this.todos = [...this.todos, res.data]
-      ).catch(err => console.log(err));
-    }
-  },
-  created() {
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5').then(res => this.todos = res.data
-    ).catch(error => console.log(error));
+    Header
   }
+  
 }
 </script>
 
