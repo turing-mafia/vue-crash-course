05fb786f1cd1ed952c98a72e85c75634d14ef8ee
I_Jemin <i_jemin@icloud.com>
2019-10-13 00:43:13 +0900

    jsonplaceholder를 통해 가상의 백엔드를 통한 추가/삭제 기능 구현

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,45 @@
 <template>
   <div id="app">
+    <Header />
+    <AddTodo v-on:add-todo="addTodo"/>
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos'
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import axios from 'axios';
 
 export default {
   name: 'app',
   components: {
-    Todos
+    Header,
+    Todos,
+    AddTodo
   },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One", 
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo Two", 
-          completed: true
-        },
-        {
-          id: 3,
-          title: "Todo Three", 
-          completed: false
-        }
-      ]
+      todos: []
     }
   },
   methods: {
     deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
+      axios.delete('https://jsonplaceholder.typicode.com/todos/${id}').then(
+        res => this.todos = this.todos.filter(todo => todo.id !== id)
+      ).catch(err => console.log(err));
+    },
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
+      axios.post('https://jsonplaceholder.typicode.com/todos', {title, completed}).then(
+        res => this.todos = [...this.todos, res.data]
+      ).catch(err => console.log(err));
     }
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5').then(res => this.todos = res.data
+    ).catch(error => console.log(error));
   }
 }
 </script>
@@ -53,4 +56,16 @@ body {
   line-height: 1.4;
 }
 
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
+}
 </style>
