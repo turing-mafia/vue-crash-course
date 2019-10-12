04f805dbe41fecac3417a2c030424e7f13372839
I_Jemin <i_jemin@icloud.com>
2019-10-12 23:40:42 +0900

    Todo를 메인 페이지에 랜더링
    - Todo 객체 수만큼 TodoItem 뷰를 랜더링
    - 완료한 Todo는 취소선 스타일 적용

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,12 +1,14 @@
 <template>
-    <div>
-        <p>Todo Item</p>
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+        <p>Hi</p>
+        <p>{{todo.title}}</p>
     </div>
 </template>
 
 <script>
 export default {
-    name: "TodoItem"
+    name: "TodoItem",
+    props: ["todo"]
 }
 </script>
 
